b2916806915e5780485cd2dc8c1ab8bdb711ead1
NeterOster <neteroster@gmail.com>
2022-08-10 20:51:55 +0800

    Update tools.rs

--- tools.rs
+++ tools.rs
@@ -1,3 +1,5 @@
+use std::{fmt::Debug, io::Write, vec};
+
 #[macro_export]
 macro_rules! measuret_codeblock {
     ($name:literal, $($code_block:tt)+) => {
@@ -25,3 +27,46 @@ pub fn measuret_closure<T>(f: impl FnOnce() -> T) -> (T, std::time::Duration) {
  * example: `let (_, t) = measuret_closure(|| {let mut x = 0; for i in 0..10000 {x += 1;}});`
 */
 
+pub struct Dumper {
+    pub path: &'static str,
+    pub file: std::fs::File,
+}
+
+impl Dumper {
+    pub fn new(path: &'static str) -> Dumper {
+        Dumper {
+            path,
+            file: std::fs::File::options()
+                .write(true)
+                .create(true)
+                .append(true)
+                .open(path)
+                .unwrap(),
+        }
+    }
+    pub fn dump_line(&mut self, content: &str) {
+        self.file.write_fmt(format_args!("{}\n", content)).unwrap();
+    }
+    pub fn dump_debug_obj<T: Debug>(&mut self, obj: T) {
+        self.file.write_fmt(format_args!("{:?}\n", obj)).unwrap();
+    }
+    pub fn dump_closure_exetime<T>(&mut self, name: &str, f: impl FnOnce() -> T) -> T {
+        let (res, exetime) = measuret_closure(f);
+        self.dump_line(format!("+ '{}' exec time: {:?}", name, exetime).as_str());
+        res
+    }
+    pub fn dump_closure_result<T: Debug>(&mut self, name: &str, f: impl FnOnce() -> T) -> T {
+        let res = f();
+        self.dump_line(format!("+ '{}' result: {:?}", name, res).as_str());
+        res
+    }
+    pub fn dump_closure_exetime_result<T: Debug>(
+        &mut self,
+        name: &str,
+        f: impl FnOnce() -> T,
+    ) -> T {
+        let (res, exetime) = measuret_closure(f);
+        self.dump_line(format!("+ '{}' result: {:?}; exetime: {:?}", name, res, exetime).as_str());
+        res
+    }
+}
